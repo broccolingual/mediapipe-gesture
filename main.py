614dc488821aab950b44a8a8a9a77e8128040b6f
Broccolingual <broccolingual@gmail.com>
2022-09-29 10:03:35 +0900

    add: add past pose func

--- main.py
+++ main.py
@@ -40,8 +40,10 @@ if __name__ == "__main__":
     cv2.setWindowProperty("Pose", cv2.WND_PROP_FULLSCREEN,
                           cv2.WINDOW_FULLSCREEN)
 
-    past_landmarks_num = 60  # past frame num
+    blDebug = False
+    past_landmarks_num = 15  # past frame num
     past_landmarks = [] * past_landmarks_num  # 0:latest
+    past_poses = [] * past_landmarks_num  # 0:latest
 
     with mp_pose.Pose(
             min_detection_confidence=0.6,
@@ -60,72 +62,58 @@ if __name__ == "__main__":
                 continue
             landmarks = results.pose_landmarks.landmark
             past_landmarks.insert(0, landmarks)
-            if len(past_landmarks) >= past_landmarks_num:
-                del past_landmarks[-1]
 
             frame.flags.writeable = True
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-            for i, landmark in enumerate(landmarks):
-                if landmark.visibility < 0.5:
-                    continue
-
-                if i == 0:  # nose
-                    pass
-
-                elif i == 11:  # left shoulder
-                    pass
-
-                elif i == 12:  # right shoulder
-                    pass
-
-                elif i == 13:  # left elbow
-                    pass
-
-                elif i == 14:  # right elbow
-                    pass
-
-                elif i == 15:  # left wrist
-                    pass
-
-                elif i == 16:  # right wrist
-                    pass
-
-            # mosaic
-            frame = mosaicArea(
-                frame, int(landmarks[0].x * w-128), int(landmarks[0].y * h - 128), 256, 256)
+            poseTmp = None
 
             # nose
-            cv2.putText(
-                frame,
-                f"Nose pos x: {round(landmarks[0].x, 2)} y: {round(landmarks[0].y, 2)} z: {round(landmarks[0].z, 2)}",
-                (12, 20), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
+            nose = landmarks[0]
+
+            if nose.visibility > 0.5:
+                # mosaic
+                frame = mosaicArea(
+                    frame, int(nose.x * w-128), int(nose.y * h - 128), 256, 256)
+
+                # nose
+                cv2.putText(
+                    frame,
+                    f"Nose pos x: {round(nose.x, 2)} y: {round(nose.y, 2)} z: {round(nose.z, 2)}",
+                    (12, 20), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
 
             # body degree
             ls = landmarks[11]  # left shoulder
             rs = landmarks[12]  # right shoulder
 
-            deg = getDegree(rs.x, rs.y, ls.x, ls.y)
-            cv2.putText(
-                frame,
-                f"Body deg {round(deg, 1)} deg",
-                (12, 48), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
+            if ls.visibility > 0.5 and rs.visibility > 0.5:
+                deg = getDegree(rs.x, rs.y, ls.x, ls.y)
+                cv2.putText(
+                    frame,
+                    f"Body deg {round(deg, 1)} deg",
+                    (12, 48), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
 
-            if abs(deg) <= 20:
-                cv2.putText(
-                    frame,
-                    "Center",
-                    (int(w/2) - 64, int(h/2) + 36), cv2.FONT_ITALIC, 1, (0, 255, 0), 4)
-            elif deg < -20:
-                cv2.putText(
-                    frame,
-                    "Left",
-                    (12, int(h/2) + 36), cv2.FONT_ITALIC, 1, (0, 255, 0), 4)
-            elif deg > 20:
-                cv2.putText(
-                    frame,
-                    "Right",
-                    (int(w) - 128, int(h/2) + 36), cv2.FONT_ITALIC, 1, (0, 255, 0), 4)
+                if abs(deg) <= 20:
+                    if blDebug:
+                        cv2.putText(
+                            frame,
+                            "Center",
+                            (int(w/2) - 64, int(h/2) + 36), cv2.FONT_ITALIC, 1, (0, 255, 0), 4)
+                    poseTmp = "Body center"
+                elif deg < -20:
+                    if blDebug:
+                        cv2.putText(
+                            frame,
+                            "Left",
+                            (12, int(h/2) + 36), cv2.FONT_ITALIC, 1, (0, 255, 0), 4)
+                    poseTmp = "Body left"
+                elif deg > 20:
+                    if blDebug:
+                        cv2.putText(
+                            frame,
+                            "Right",
+                            (int(w) - 128, int(h/2) + 36), cv2.FONT_ITALIC, 1, (0, 255, 0), 4)
+                    poseTmp = "Body right"
 
             # arms
             l0 = landmarks[13]
@@ -138,41 +126,73 @@ if __name__ == "__main__":
             l_cos = ((lw.x - l0.x) * (ls.x - l0.x) +
                      (lw.y - l0.y) * (ls.y - l0.y)) / (math.sqrt((lw.x - l0.x) ** 2 + (lw.y - l0.y) ** 2) * math.sqrt((ls.x - l0.x) ** 2 + (ls.y - l0.y) ** 2))
 
-            cv2.putText(
-                frame,
-                f"Right Arm Cos {round(r_cos, 2)}",
-                (12, 72), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
-
-            cv2.putText(
-                frame,
-                f"Left Arm Cos  {round(l_cos, 2)}",
-                (12, 96), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
-
-            if -0.85 < l_cos < 0.85:
+            if r0.visibility > 0.5 and rw.visibility > 0.5:
                 cv2.putText(
                     frame,
-                    "Right",
-                    (int(w) - 96, int(h/2)), cv2.FONT_ITALIC, 1, (0, 0, 255), 4)
-                cv2.putText(
-                    frame,
-                    "Straight",
-                    (int(w) - 96, int(h/2) + 24), cv2.FONT_ITALIC, 0.6, (0, 0, 255), 2)
+                    f"Right Arm Cos {round(r_cos, 2)}",
+                    (12, 72), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
 
-            if -0.85 < r_cos < 0.85:
-                cv2.putText(
-                    frame,
-                    "Left",
-                    (12, int(h/2)), cv2.FONT_ITALIC, 1, (0, 0, 255), 4)
-                cv2.putText(
-                    frame,
-                    "Straight",
-                    (12, int(h/2) + 24), cv2.FONT_ITALIC, 0.6, (0, 0, 255), 2)
+                if -0.85 < l_cos < 0.85:
+                    if blDebug:
+                        cv2.putText(
+                            frame,
+                            "Right",
+                            (int(w) - 96, int(h/2)), cv2.FONT_ITALIC, 1, (0, 0, 255), 4)
+                        cv2.putText(
+                            frame,
+                            "Straight",
+                            (int(w) - 96, int(h/2) + 24), cv2.FONT_ITALIC, 0.6, (0, 0, 255), 2)
+                    poseTmp = "Right straight"
 
-            landmarks
-            mp_drawing.draw_landmarks(
-                frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
-                connection_drawing_spec=mp_drawing.DrawingSpec(
-                    thickness=3, circle_radius=20))
+            if l0.visibility > 0.5 and lw.visibility > 0.5:
+                cv2.putText(
+                    frame,
+                    f"Left Arm Cos  {round(l_cos, 2)}",
+                    (12, 96), cv2.FONT_ITALIC, 0.7, (255, 255, 255), 2)
+
+                if -0.85 < r_cos < 0.85:
+                    if blDebug:
+                        cv2.putText(
+                            frame,
+                            "Left",
+                            (12, int(h/2)), cv2.FONT_ITALIC, 1, (0, 0, 255), 4)
+                        cv2.putText(
+                            frame,
+                            "Straight",
+                            (12, int(h/2) + 24), cv2.FONT_ITALIC, 0.6, (0, 0, 255), 2)
+                    poseTmp = "Left straight"
+
+            past_poses.insert(0, poseTmp)
+            if len(past_poses) >= past_landmarks_num:
+                cntDict = {}
+                for ps in past_poses:
+                    if ps in cntDict:
+                        cntDict[ps] += 1
+                    else:
+                        cntDict[ps] = 1
+
+                cntMax = 0
+                poseMax = ""
+                for ps, cnt in cntDict.items():
+                    if cntMax < cnt:
+                        cntMax = cnt
+                        poseMax = ps
+
+                cv2.putText(
+                    frame,
+                    f"[{poseMax}] ({cntMax}/{past_landmarks_num} frames)",
+                    (int(w/2) - 128, int(h) - 36), cv2.FONT_ITALIC, 0.6, (255, 0, 0), 2)
+
+            if len(past_landmarks) >= past_landmarks_num:
+                del past_landmarks[-1]
+            if len(past_poses) >= past_landmarks_num:
+                del past_poses[-1]
+
+            if blDebug:
+                mp_drawing.draw_landmarks(
+                    frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                    connection_drawing_spec=mp_drawing.DrawingSpec(
+                        thickness=3, circle_radius=20))
 
             cv2.imshow("Pose", frame)
 
